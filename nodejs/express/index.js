fecb57aeccf4b5d65ce49d12da119e7eb1629247
bsenk <bsenk77@gmail.com>
2023-02-01 15:59:12 +0200

    nothing

--- nodejs/express/index.js
+++ nodejs/express/index.js
@@ -1,9 +1,34 @@
+const { MongoClient } = require("mongodb")
 const express = require("express")
-
 const app = express();
+const client = new MongoClient("mongodb+srv://bsenk:senichak26275@cluster0.om1ex0f.mongodb.net/?retryWrites=true&w=majority")
+const start = async () => {
+    try{
+        await client.connect()
+        console.log("gotovo");
+        const users = client.db().collection("users")
+        // await users.insertOne({name: "bsenk", age: 22})
+        // const user = await users.findOne({age: 22})
+        // console.log(user);
+    } catch(e) {
+        console.log(e);
+    }
+}
+start();
 
-app.get("/gg", (req,res)=>{
-    res.send(JSON.stringify([1,3,5,6]))
+app.get("/gg", async (req,res)=>{
+    await client.connect()
+    const answer = await client.db().collection("users").findOne({age: 22})
+    console.log(req.body)
+    res.send(answer)
 })
+
+app.post("/gh" , async (req,res)=>{
+    await client.connect()
+    const answer = await client.db().collection("users").insertOne()
+    console.log(req)
+    res.send(answer)
+})
+
 
 app.listen(3000)
