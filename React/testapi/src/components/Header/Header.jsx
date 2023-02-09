50ae85f4c11474906af9cb4d9d5088ef16d5cecc
bsenk <bsenk77@gmail.com>
2023-02-09 20:43:22 +0200

    Make cart

--- React/testapi/src/components/Header/Header.jsx
+++ React/testapi/src/components/Header/Header.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import s from "./Header.module.css"
+import s from "./Header.module.css";
+import { BsCart2 } from "react-icons/bs";
+import { FiUser } from "react-icons/fi";
+import { toggleCartState } from "../../store/cart/cartActions";
+import Cart from "../Cart/Cart";
 
 export const Header = (props) => {
-  return (
-    <header className={s.header}><Link to={"/"}>
-      <img src="/images/logo.png" alt="logo" />
-        Fake store
-      </Link></header>
-  )
-}
+  const dispatch = useDispatch();
+  const cartState = useSelector((state) => state.cart.cartState);
+  const cartItems = useSelector((state) => state.cart.cartItems);
 
-export default connect()(Header)
+  return (
+    <header className={s.header}>
+      <div className={s.blank}></div>
+      <Link to={"/"}>
+        <img src="/images/logo.png" alt="logo" />
+        Fake store
+      </Link>
+      <div className={s.userBar}>
+        <div className={s.btn}>
+          <BsCart2 onClick={() => dispatch(toggleCartState())} />
+          {cartState && <Cart />}
+          {cartItems.length !== 0 && (
+            <div className={s.cartCounter}>{cartItems.length}</div>
+          )}
+        </div>
+        <div className={s.btn}>
+          <FiUser />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default connect()(Header);
