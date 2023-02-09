40242c91e8ebaee608d7da6797fb4e05a9694bb0
bsenk <bsenk77@gmail.com>
2023-02-09 20:50:19 +0200

    fix localStorage

--- React/testapi/src/App.js
+++ React/testapi/src/App.js
@@ -2,30 +2,29 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Product from "./Product/Product";
 import Products from "./Products/Products";
-import Header from "./components/Header/Header"
-import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCartItems } from "./store/cart/cartActions";
 
-
 function App() {
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    dispatch(setCartItems(JSON.parse(window.localStorage.getItem("cart"))))
-  })
-  const link = useLocation()
-  console.log(link)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    window.localStorage.getItem("cart") !== null &&
+      dispatch(setCartItems(JSON.parse(window.localStorage.getItem("cart"))));
+  });
+  const link = useLocation();
+  console.log(link);
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Products/>}/>
-        <Route path='/product/:id' element={<Product/>}/>
+        <Route path="/" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-    
   );
 }
 
